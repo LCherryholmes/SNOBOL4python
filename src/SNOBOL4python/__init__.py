8f3b83d227d7b2a93edaec2130100a42c54527e1
LCherryholmes <lcherryh@yahoo.com>
2025-01-27 22:51:07 -0600

    More better!!!

--- src/SNOBOL4python/__init__.py
+++ src/SNOBOL4python/__init__.py
@@ -1,11 +1,14 @@
-from .SNOBOL4 import pos, subject, results, PATTERN, pattern, assign
+from .SNOBOL4 import pos, subject, PATTERN, pattern, assign
 from .SNOBOL4 import _, ε, _UCASE, _LCASE, _digits
-from .SNOBOL4 import ANY, ARBNO, BREAK, FAIL, FENCE, LEN, LIT, NOTANY
-from .SNOBOL4 import POS, REM, RPOS, RTAB, SPAN, SUCCESS, TAB
+from .SNOBOL4 import ANY, ARBNO, BREAK, DIFFER, FAIL, FENCE, IDENT, INTEGER
+from .SNOBOL4 import LEN, LIT, NOTANY, POS, REM, RPOS, RTAB, SPAN, SUCCESS, TAB
 from .SNOBOL4 import AND, ALT, SEQ, MATCH
+from .SNOBOL4 import EQ, LT, GT, NE, LE, GE
+from .SNOBOL4 import eq, lt, gt, ne, le, ge
+from .SNOBOL4 import LEQ, LLT, LGT, LNE, LLE, LGE
 
 __all__ = [
-    "pos", "subject", "results", "PATTERN", "pattern", "assign",
+    "pos", "subject", "PATTERN", "pattern", "assign",
     "_", "ε", "_UCASE", "_LCASE", "_digits",
     "ANY", "ARBNO", "BREAK", "FAIL", "FENCE", "LEN", "LIT", "NOTANY",
     "POS", "REM", "RPOS", "RTAB", "SPAN", "SUCCESS", "TAB",
