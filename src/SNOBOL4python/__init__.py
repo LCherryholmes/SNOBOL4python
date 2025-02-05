cbeb9cc6d23b803cc4d1f2ff9f6c01788836034e
LCherryholmes <lcherryh@yahoo.com>
2025-02-05 01:10:56 -0600

    Immediate and conditional assignments.

--- src/SNOBOL4python/__init__.py
+++ src/SNOBOL4python/__init__.py
@@ -1,19 +1,21 @@
-from .SNOBOL4 import PATTERN, pattern, pos, subject
+#-------------------------------------------------------------------------------
 from .SNOBOL4 import SEARCH, MATCH, FULLMATCH
-from .SNOBOL4 import _ALPHABET, _UCASE, _LCASE, _digits
-from .SNOBOL4 import ε, σ, Σ, Π, π, Ξ, Δ, δ, Λ, λ
-from .SNOBOL4 import ABORT, ANY, ARB, ARBNO, BAL, BREAK, DIFFER, FAIL, FENCE
-from .SNOBOL4 import IDENT, INTEGER, LEN, NOTANY, POS, REM, RPOS
+from .SNOBOL4 import PATTERN, pattern, _ALPHABET, _UCASE, _LCASE, _digits
+from .SNOBOL4 import ε, σ, Σ, Π, π, Ξ, Δ, δ, Λ, λ, θ
+from .SNOBOL4 import ABORT, ANY, ARB, ARBNO, BAL, BREAK, DIFFER, FAIL
+from .SNOBOL4 import FENCE, IDENT, INTEGER, LEN, NOTANY, POS, REM, RPOS
 from .SNOBOL4 import RTAB, SPAN, SUCCESS, TAB
 from .SNOBOL4 import nPush, nInc, nPop, Shift, Reduce
-
+from .SNOBOL4 import JSONDecode, _shift, _reduce
+#-------------------------------------------------------------------------------
 __all__ = [
-    "PATTERN", "pattern", "pos", "subject",
-    "ε", "σ", "Σ", "Π", "π", "Ξ", "Δ", "δ" "Λ", "λ",
     "SEARCH", "MATCH", "FULLMATCH",
-    "_ALPHABET", "_UCASE", "_LCASE", "_digits",
-    "ABORT", "ANY", "ARB", "ARBNO", "BAL", "BREAK", "DIFFER", "FAIL", "FENCE",
-    "IDENT", "INTEGER", "LEN", "NOTANY", "POS", "REM", "RPOS",
+    "PATTERN", "pattern", "_ALPHABET", "_UCASE", "_LCASE", "_digits",
+    "ε", "σ", "Σ", "Π", "π", "Ξ", "Δ", "δ" "Λ", "λ", "θ",
+    "ABORT", "ANY", "ARB", "ARBNO", "BAL", "BREAK", "DIFFER", "FAIL",
+    "FENCE", "IDENT", "INTEGER", "LEN", "NOTANY", "POS", "REM", "RPOS",
     "RTAB", "SPAN", "SUCCESS", "TAB",
-    "nPush", "nInc", "nPop", "Shift", "Reduce"
+    "nPush", "nInc", "nPop", "Shift", "Reduce",
+    "JSONDecode", "_shift", "_reduce"
 ]
+#-------------------------------------------------------------------------------
