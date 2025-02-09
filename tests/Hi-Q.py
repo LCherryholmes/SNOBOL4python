8008ff74687fc50c400e40e79a255dad433798d3
LCherryholmes <lcherryh@yahoo.com>
2025-02-09 16:08:09 -0600

    Tried single-dimension 'bytearray'.

--- tests/Hi-Q.py
+++ tests/Hi-Q.py
@@ -1,19 +1,5 @@
-B = [bytearray.fromhex('FFFF010101FFFF'),
-     bytearray.fromhex('FFFF010101FFFF'),
-     bytearray.fromhex('01010101010101'),
-     bytearray.fromhex('01010100010101'),
-     bytearray.fromhex('01010101010101'),
-     bytearray.fromhex('FFFF010101FFFF'),
-     bytearray.fromhex('FFFF010101FFFF')]
-
-W = [bytearray.fromhex('FFFF000000FFFF'),
-     bytearray.fromhex('FFFF000000FFFF'),
-     bytearray.fromhex('00000000000000'),
-     bytearray.fromhex('00000001000000'),
-     bytearray.fromhex('00000000000000'),
-     bytearray.fromhex('FFFF000000FFFF'),
-     bytearray.fromhex('FFFF000000FFFF')]
-
+B = bytearray.fromhex('FFFF010101FFFFFFFF010101FFFF010101010101010101010001010101010101010101FFFF010101FFFFFFFF010101FFFF')
+W = bytearray.fromhex('FFFF000000FFFFFFFF000000FFFF000000000000000000000100000000000000000000FFFF000000FFFFFFFF000000FFFF')
 trials =  (       (0,2),
                   (1,2),
     (2,0), (2,1), (2,2), (2,3), (2,4),
@@ -23,25 +9,25 @@ trials =  (       (0,2),
                   (6,2),)
 
 def make_jump(j): return (j[0] ^ 1, j[1] ^ 1, j[2] ^ 1)
-def set_horizontal (r, c, j): B[r][c+0] = j[0]; \
-                              B[r][c+1] = j[1]; \
-                              B[r][c+2] = j[2]
-def set_vertical   (r, c, j): B[r+0][c] = j[0]; \
-                              B[r+1][c] = j[1]; \
-                              B[r+2][c] = j[2]
-def is_horizontal  (r, c, j): return B[r][c+0] == j[0] \
-                                 and B[r][c+1] == j[1] \
-                                 and B[r][c+2] == j[2]
-def is_vertical    (r, c, j): return B[r+0][c] == j[0] \
-                                 and B[r+1][c] == j[1] \
-                                 and B[r+2][c] == j[2]
+def set_horizontal (r, c, j): B[r * 7 + c+0] = j[0]; \
+                              B[r * 7 + c+1] = j[1]; \
+                              B[r * 7 + c+2] = j[2]
+def set_vertical   (r, c, j): B[(r+0) * 7 + c] = j[0]; \
+                              B[(r+1) * 7 + c] = j[1]; \
+                              B[(r+2) * 7 + c] = j[2]
+def is_horizontal  (r, c, j): return B[r * 7 + c+0] == j[0] \
+                                 and B[r * 7 + c+1] == j[1] \
+                                 and B[r * 7 + c+2] == j[2]
+def is_vertical    (r, c, j): return B[(r+0) * 7 + c] == j[0] \
+                                 and B[(r+1) * 7 + c] == j[1] \
+                                 and B[(r+2) * 7 + c] == j[2]
 
 attempts = 0
 def print_board(board, n, attempts=None):
     print(f"{n} ({attempts:,}):\n")
     for r in range(0, 7):
         for c in range(0, 7):
-            match board[r][c]:
+            match board[r * 7 + c]:
                 case 0: print(" .", end='')
                 case 1: print(" o", end='')
                 case 255: print("  ", end='')
