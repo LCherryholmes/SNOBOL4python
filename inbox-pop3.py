44d11d2549bb16c31ec0fd5c784b65ce7bf8d5c1
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-02-28 21:50:04 -0600

    BEGIN boot-strapping

---
+++ inbox-pop3.py
@@ -0,0 +1,102 @@
+yield from (
+inboxFrom() + η() +
+ς('X-Account-Key:') + ς(' ') + ς('account1') + η() +
+ς('X-UIDL:') + ς(' ') + ς('ABVlxEIAAC5GRXQn/Ajb40l5eAU') + η() +
+ς('X-Mozilla-Status:') + ς(' ') + ς('0000') + η() +
+ς('X-Mozilla-Status2:') + ς(' ') + ς('00000000') + η() +
+ς('X-Mozilla-Keys:') + ς('                                                                                 ') + η() +
+ς('X-Apparently-To:') + ς(' ') + ς('lcherryh@yahoo.com') + ς(' ') + ς('via') + ς(' ') + ς('66.196.101.21;') + ς(' ') + ς('Mon,') + ς(' ') + ς('04') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('05:51:56') + ς(' ') + ς('-0800') + η() +
+ς('X-Originating-IP:') + ς(' ') + ς('[12.106.87.68]') + η() +
+ς('Return-Path:') + ς(' ') + ς('<ken.moses@quest.com>') + η() +
+ς('Authentication-Results:') + ς(' ') + ς('mta526.mail.mud.yahoo.com') + ς('  ') + ς('from=quest.com;') + ς(' ') + ς('domainkeys=neutral') + ς(' ') + ς('(no') + ς(' ') + ς('sig)') + η() +
+ς('Received:') + ς(' ') + ς('from') + ς(' ') + ς('12.106.87.68') + ς('  ') + ς('(EHLO') + ς(' ') + ς('irvbhxw02.quest.com)') + ς(' ') + ς('(12.106.87.68)') + η() +
+ς('  ') + ς('by') + ς(' ') + ς('mta526.mail.mud.yahoo.com') + ς(' ') + ς('with') + ς(' ') + ς('SMTP;') + ς(' ') + ς('Mon,') + ς(' ') + ς('04') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('05:51:56') + ς(' ') + ς('-0800') + η() +
+ς('Received:') + ς(' ') + ς('from') + ς(' ') + ς('alvmbxw02.prod.quest.corp') + ς(' ') + ς('([10.1.0.209])') + ς(' ') + ς('by') + ς(' ') + ς('irvbhxw02.quest.com') + ς(' ') + ς('with') + ς(' ') + ς('Microsoft') + ς(' ') + ς('SMTPSVC(6.0.3790.1830);') + η() +
+μ() + ς(' ') + ς('Mon,') + ς(' ') + ς('4') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('05:51:56') + ς(' ') + ς('-0800') + η() +
+ς('X-MimeOLE:') + ς(' ') + ς('Produced') + ς(' ') + ς('By') + ς(' ') + ς('Microsoft') + ς(' ') + ς('Exchange') + ς(' ') + ς('V6.5') + η() +
+ς('Content-class:') + ς(' ') + ς('urn:content-classes:message') + η() +
+ς('MIME-Version:') + ς(' ') + ς('1.0') + η() +
+ς('Content-Type:') + ς(' ') + ς('multipart/alternative;') + η() +
+μ() + ς('boundary="----_=_NextPart_001_01C717AB.5C51AFDE"') + η() +
+ς('Subject:') + ς(' ') + ς('Out') + ς(' ') + ς('of') + ς(' ') + ς('Office') + ς(' ') + ς('AutoReply:') + ς(' ') + ς('Account') + ς(' ') + ς('accidentally') + ς(' ') + ς('disabled!') + η() +
+ς('Date:') + ς(' ') + ς('Mon,') + ς(' ') + ς('4') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('21:51:55') + ς(' ') + ς('+0800') + η() +
+ς('Message-ID:') + ς(' ') + ς('<78CD474EB76D9C4C9E8F03720184A36B0B5BCBF2@ALVMBXW02.prod.quest.corp>') + η() +
+ς('X-MS-Has-Attach:') + ς(' ') + η() +
+ς('X-MS-TNEF-Correlator:') + ς(' ') + η() +
+ς('Thread-Topic:') + ς(' ') + ς('Account') + ς(' ') + ς('accidentally') + ς(' ') + ς('disabled!') + η() +
+ς('Thread-Index:') + ς(' ') + ς('AccXq1xKC7R4pDK/Q6KSUBrhS+yX+QAAAAFe') + η() +
+ς('From:') + ς(' ') + ς('"Ken') + ς(' ') + ς('Moses"') + ς(' ') + ς('<Ken.Moses@quest.com>') + η() +
+ς('To:') + ς(' ') + ς('"Lon') + ς(' ') + ς('Cherryholmes"') + ς(' ') + ς('<lcherryh@yahoo.com>') + η() +
+ς('Return-Path:') + ς(' ') + ς('Ken.Moses@quest.com') + η() +
+ς('X-OriginalArrivalTime:') + ς(' ') + ς('04') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('13:51:56.0083') + ς(' ') + ς('(UTC)') + ς(' ') + ς('FILETIME=[5C6CD030:01C717AB]') + η() +
+ς('Content-Length:') + ς(' ') + ς('1150') + η() +
+η() +
+ς('This') + ς(' ') + ς('is') + ς(' ') + ς('a') + ς(' ') + ς('multi-part') + ς(' ') + ς('message') + ς(' ') + ς('in') + ς(' ') + ς('MIME') + ς(' ') + ς('format.') + η() +
+η() +
+ς('------_=_NextPart_001_01C717AB.5C51AFDE') + η() +
+ς('Content-Type:') + ς(' ') + ς('text/plain;') + η() +
+μ() + ς('charset="iso-8859-1"') + η() +
+ς('Content-Transfer-Encoding:') + ς(' ') + ς('quoted-printable') + η() +
+η() +
+ς('I') + ς(' ') + ς('am') + ς(' ') + ς('out') + ς(' ') + ς('of') + ς(' ') + ς('the') + ς(' ') + ς('office') + ς(' ') + ς('and') + ς(' ') + ς('will') + ς(' ') + ς('be') + ς(' ') + ς('returning') + ς(' ') + ς('on') + ς(' ') + ς('Thursday.') + ς(' ') + ς('December') + ς(' ') + ς('7.') + ς('  ') + ς('=') + η() +
+ς('For') + ς(' ') + ς('immediate') + ς(' ') + ς('HR') + ς(' ') + ς('assistance,') + ς(' ') + ς('please') + ς(' ') + ς('contact') + ς(' ') + ς('Lisa') + ς(' ') + ς('Radford') + ς(' ') + ς('at') + ς(' ') + ς('(949)') + ς(' ') + ς('=') + η() +
+ς('754-8509') + ς(' ') + ς('or') + ς(' ') + ς('Chris') + ς(' ') + ς('Ferrales') + ς(' ') + ς('at') + ς('  ') + ς('(630)') + ς(' ') + ς('836-3166') + ς(' ') + ς('x4166') + η() +
+η() +
+ς('------_=_NextPart_001_01C717AB.5C51AFDE') + η() +
+ς('Content-Type:') + ς(' ') + ς('text/html;') + η() +
+μ() + ς('charset="iso-8859-1"') + η() +
+ς('Content-Transfer-Encoding:') + ς(' ') + ς('quoted-printable') + η() +
+η() +
+ς('<!DOCTYPE') + ς(' ') + ς('HTML') + ς(' ') + ς('PUBLIC') + ς(' ') + ς('"-//W3C//DTD') + ς(' ') + ς('HTML') + ς(' ') + ς('3.2//EN">') + η() +
+ς('<HTML>') + η() +
+ς('<HEAD>') + η() +
+ς('<META') + ς(' ') + ς('HTTP-EQUIV=3D"Content-Type"') + ς(' ') + ς('CONTENT=3D"text/html;') + ς(' ') + ς('=') + η() +
+ς('charset=3Diso-8859-1">') + η() +
+ς('<META') + ς(' ') + ς('NAME=3D"Generator"') + ς(' ') + ς('CONTENT=3D"MS') + ς(' ') + ς('Exchange') + ς(' ') + ς('Server') + ς(' ') + ς('version') + ς(' ') + ς('=') + η() +
+ς('6.5.7638.1">') + η() +
+ς('<TITLE>Out') + ς(' ') + ς('of') + ς(' ') + ς('Office') + ς(' ') + ς('AutoReply:') + ς(' ') + ς('Account') + ς(' ') + ς('accidentally') + ς(' ') + ς('disabled!</TITLE>') + η() +
+ς('</HEAD>') + η() +
+ς('<BODY>') + η() +
+ς('<!--') + ς(' ') + ς('Converted') + ς(' ') + ς('from') + ς(' ') + ς('text/plain') + ς(' ') + ς('format') + ς(' ') + ς('-->') + η() +
+η() +
+ς('<P><FONT') + ς(' ') + ς('SIZE=3D2>I') + ς(' ') + ς('am') + ς(' ') + ς('out') + ς(' ') + ς('of') + ς(' ') + ς('the') + ς(' ') + ς('office') + ς(' ') + ς('and') + ς(' ') + ς('will') + ς(' ') + ς('be') + ς(' ') + ς('returning') + ς(' ') + ς('on') + ς(' ') + ς('=') + η() +
+ς('Thursday.') + ς(' ') + ς('December') + ς(' ') + ς('7.&nbsp;') + ς(' ') + ς('For') + ς(' ') + ς('immediate') + ς(' ') + ς('HR') + ς(' ') + ς('assistance,') + ς(' ') + ς('please') + ς(' ') + ς('contact') + ς(' ') + ς('=') + η() +
+ς('Lisa') + ς(' ') + ς('Radford') + ς(' ') + ς('at') + ς(' ') + ς('(949)') + ς(' ') + ς('754-8509') + ς(' ') + ς('or') + ς(' ') + ς('Chris') + ς(' ') + ς('Ferrales') + ς(' ') + ς('at&nbsp;') + ς(' ') + ς('(630)') + ς(' ') + ς('836-3166') + ς(' ') + ς('=') + η() +
+ς('x4166</FONT></P>') + η() +
+η() +
+ς('</BODY>') + η() +
+ς('</HTML>') + η() +
+ς('------_=_NextPart_001_01C717AB.5C51AFDE--') + η() +
+η() +
+inboxFrom() + η() +
+ς('X-Account-Key:') + ς(' ') + ς('account1') + η() +
+ς('X-UIDL:') + ς(' ') + ς('ABBlxEIAABVkRXRs6Q6ThBqYacU') + η() +
+ς('X-Mozilla-Status:') + ς(' ') + ς('0000') + η() +
+ς('X-Mozilla-Status2:') + ς(' ') + ς('00000000') + η() +
+ς('X-Mozilla-Keys:') + ς('                                                                                 ') + η() +
+ς('X-Apparently-To:') + ς(' ') + ς('lcherryh@yahoo.com') + ς(' ') + ς('via') + ς(' ') + ς('66.196.101.16;') + ς(' ') + ς('Mon,') + ς(' ') + ς('04') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('10:46:01') + ς(' ') + ς('-0800') + η() +
+ς('X-Originating-IP:') + ς(' ') + ς('[12.106.87.70]') + η() +
+ς('Return-Path:') + ς(' ') + ς('<pat.luis@quest.com>') + η() +
+ς('Authentication-Results:') + ς(' ') + ς('mta188.mail.re3.yahoo.com') + ς('  ') + ς('from=quest.com;') + ς(' ') + ς('domainkeys=neutral') + ς(' ') + ς('(no') + ς(' ') + ς('sig)') + η() +
+ς('Received:') + ς(' ') + ς('from') + ς(' ') + ς('12.106.87.70') + ς('  ') + ς('(EHLO') + ς(' ') + ς('irvbhxw03.quest.com)') + ς(' ') + ς('(12.106.87.70)') + η() +
+ς('  ') + ς('by') + ς(' ') + ς('mta188.mail.re3.yahoo.com') + ς(' ') + ς('with') + ς(' ') + ς('SMTP;') + ς(' ') + ς('Mon,') + ς(' ') + ς('04') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('10:46:01') + ς(' ') + ς('-0800') + η() +
+ς('Received:') + ς(' ') + ς('from') + ς(' ') + ς('alvmbxw01.prod.quest.corp') + ς(' ') + ς('([10.1.50.13])') + ς(' ') + ς('by') + ς(' ') + ς('irvbhxw03.quest.com') + ς(' ') + ς('with') + ς(' ') + ς('Microsoft') + ς(' ') + ς('SMTPSVC(6.0.3790.1830);') + η() +
+μ() + ς(' ') + ς('Mon,') + ς(' ') + ς('4') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('10:46:00') + ς(' ') + ς('-0800') + η() +
+ς('X-MimeOLE:') + ς(' ') + ς('Produced') + ς(' ') + ς('By') + ς(' ') + ς('Microsoft') + ς(' ') + ς('Exchange') + ς(' ') + ς('V6.5') + η() +
+ς('Content-class:') + ς(' ') + ς('urn:content-classes:message') + η() +
+ς('MIME-Version:') + ς(' ') + ς('1.0') + η() +
+ς('Content-Type:') + ς(' ') + ς('multipart/alternative;') + η() +
+μ() + ς('boundary="----_=_NextPart_001_01C717D4.712756D2"') + η() +
+ς('Subject:') + ς(' ') + ς('Quest') + ς(' ') + ς('Software') + ς(' ') + ς('-') + ς(' ') + ς('account') + ς(' ') + ς('re-activation') + η() +
+ς('Date:') + ς(' ') + ς('Mon,') + ς(' ') + ς('4') + ς(' ') + ς('Dec') + ς(' ') + ς('2006') + ς(' ') + ς('10:46:00') + ς(' ') + ς('-0800') + η() +
+ς('Message-ID:') + ς(' ') + ς('<FA13712B13469646A618BC95F7E1BA8F01ABD6E9@alvmbxw01.prod.quest.corp>') + η() +
+ς('X-MS-Has-Attach:') + ς(' ') + η() +
+ς('X-MS-TNEF-Correlator:') + ς(' ') + η() +
+ς('Thread-Topic:') + ς(' ') + ς('Quest') + ς(' ') + ς('Software') + ς(' ') + ς('-') + ς(' ') + ς('account') + ς(' ') + ς('re-activation') + η() +
+ς('Thread-Index:') + ς(' ') + ς('AccX1HFT7mXxXKMqQ4WfYy2XVAP9zw==') + η() +
+ς('From:') + ς(' ') + ς('"Pat') + ς(' ') + ς('Luis"') + ς(' ') + ς('<pat.luis@quest.com>') + η() +
+ς('To:') + ς(' ') + ς('<lcherryh@yahoo.com>') + η() +
+ς('Cc:') + ς(' ') + ς('"Ken') + ς(' ') + ς('Moses"') + ς(' ') + ς('<Ken.Moses@quest.com>,') + η() +
+μ() + ς('"Keren') + ς(' ') + ς('Kamilian"') + ς(' ') + ς('<Keren.Kamilian@quest.com>') + η() +
+ς('Return-Path:') + ς(' ') + ς('pat.luis@quest.com') + η() +
+'')
