ead0c88ac5afc35b4dccf1c701311d39122882e0
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-02-28 19:10:49 -0600

    feedback loop

--- meta-yaml.py
+++ meta-yaml.py
@@ -2,12 +2,12 @@ yield from (
 hashStyleComment() +
 hashStyleComment() +
 η() +
-ς('package') + ς(':') + η() +
+yamlStatement() + η() +
 ς('name') + ς(':') + identifier() + η() +
 ς('version') + ς(':') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + η() +
-ς('source') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + ς('md5') + ς(':') + SPAN(_DIGITS) + identifier() + η() +
-ς('patches') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + ς('patches') + ς('/') + SPAN(_DIGITS) + ς('-') + SPAN(_UCASE) + identifier() + ς('-') + SPAN(_UCASE) + identifier() + ς('-') + SPAN(_UCASE) + ς('_') + SPAN(_UCASE) + ς('-') + identifier() + ς('-') + SPAN(_DIGITS) + ς('-') + identifier() + ς('-') + SPAN(_DIGITS) + ς('.') + identifier() + η() +
 ς('-') + ς('patches') + ς('/') + SPAN(_DIGITS) + ς('-') + SPAN(_UCASE) + identifier() + ς('-') + SPAN(_UCASE) + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('.') + identifier() + η() +
 ς('-') + ς('patches') + ς('/') + SPAN(_DIGITS) + ς('-') + SPAN(_UCASE) + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('.') + identifier() + ς('-') + identifier() + ς('-') + ς('.') + identifier() + η() +
@@ -31,33 +31,33 @@ hashStyleComment() +
 ς('-') + ς('patches') + ς('/') + SPAN(_DIGITS) + ς('-') + SPAN(_UCASE) + identifier() + ς('-') + identifier() + ς('-') + SPAN(_UCASE) + ς('.') + identifier() + η() +
 ς('-') + ς('patches') + ς('/') + SPAN(_DIGITS) + ς('-') + SPAN(_UCASE) + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + ς('-') + identifier() + SPAN(_UCASE) + ς('-') + identifier() + ς('-') + identifier() + ς('.') + identifier() + η() +
 ς('url') + ς(':') + ς('https') + ς(':') + ς('/') + ς('/') + identifier() + ς('.') + identifier() + ς('.') + identifier() + ς('/') + identifier() + ς('/') + identifier() + ς('/') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('/') + SPAN(_UCASE) + identifier() + ς('-') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + identifier() + ς('.') + identifier() + η() +
-ς('build') + ς(':') + η() +
+yamlStatement() + η() +
 ς('activate_in_script') + ς(':') + identifier() + η() +
 ς('detect_binary_files_with_prefix') + ς(':') + identifier() + η() +
-ς('missing_dso_whitelist') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + stringLiteral() + η() +
 ς('-') + stringLiteral() + η() +
 ς('-') + stringLiteral() + η() +
-ς('no_link') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + SPAN(_UCASE) + identifier() + ς('/') + ς('_') + identifier() + ς('.') + identifier() + η() +
 ς('number') + ς(':') + SPAN(_DIGITS) + η() +
-ς('run_exports') + ς(':') + η() +
-ς('noarch') + ς(':') + η() +
+yamlStatement() + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + η() +
-ς('weak') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + ς('*') + ς('*') + ς('_') + identifier() + η() +
-ς('script_env') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + SPAN(_UCASE) + ς('_') + SPAN(_UCASE) + ς('=') + identifier() + η() +
 ς('-') + SPAN(_UCASE) + ς('_') + SPAN(_UCASE) + ς('_') + SPAN(_UCASE) + ς('=') + identifier() + η() +
 ς('-') + SPAN(_UCASE) + ς('_') + SPAN(_UCASE) + ς('_') + SPAN(_UCASE) + ς('_') + SPAN(_UCASE) + ς('=') + identifier() + η() +
 ς('skip_compile_pyc') + ς(':') + identifier() + η() +
 ς('string') + ς(':') + identifier() + η() +
-ς('requirements') + ς(':') + η() +
-ς('build') + ς(':') + η() +
+yamlStatement() + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + ς('-') + SPAN(_DIGITS) + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + ς('-') + SPAN(_DIGITS) + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
-ς('host') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
@@ -74,7 +74,7 @@ hashStyleComment() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
-ς('run') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + ς('>') + ς('=') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς(',') + ς('<') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + ς('>') + ς('=') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς(',') + ς('<') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
 ς('-') + identifier() + ς('>') + ς('=') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς(',') + ς('<') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
@@ -87,10 +87,10 @@ hashStyleComment() +
 ς('-') + identifier() + ς('>') + ς('=') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς(',') + ς('<') + SPAN(_DIGITS) + η() +
 ς('-') + identifier() + ς('>') + ς('=') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + η() +
 ς('-') + identifier() + ς('>') + ς('=') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς(',') + ς('<') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + identifier() + η() +
-ς('run_constrained') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + ς('.') + ς('*') + ς('*') + ς('_') + identifier() + η() +
-ς('test') + ς(':') + η() +
-ς('commands') + ς(':') + η() +
+yamlStatement() + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + identifier() + η() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + ς('-') + SPAN(_UCASE) + η() +
@@ -124,21 +124,21 @@ hashStyleComment() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + identifier() + ς('.') + identifier() + η() +
 ς('-') + identifier() + ς('-') + identifier() + ς('"') + identifier() + identifier() + identifier() + SPAN(_UCASE) + ς(';') + SPAN(_UCASE) + ς('(') + SPAN(_UCASE) + identifier() + ς(')') + ς('(') + identifier() + ς(')') + ς('"') + η() +
-ς('downstreams') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + η() +
-ς('files') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + ς('.') + identifier() + η() +
 ς('-') + identifier() + ς('/') + identifier() + ς('/') + ς('*') + η() +
 ς('-') + identifier() + ς('/') + identifier() + ς('/') + ς('*') + η() +
 ς('-') + identifier() + ς('/') + identifier() + ς('-') + identifier() + ς('/') + ς('*') + η() +
-ς('requires') + ς(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + ς('-') + SPAN(_DIGITS) + η() +
-ς('about') + ς(':') + η() +
-ς('description') + ς(':') + stringLiteral() + η() +
+yamlStatement() + η() +
+yamlStatement() + η() +
 ς('dev_url') + σ(':') + ς('https') + ς(':') + ς('/') + ς('/') + identifier() + ς('.') + identifier() + ς('.') + identifier() + ς('/') + identifier() + ς('/') + η() +
 ς('doc_source_url') + σ(':') + ς('https') + ς(':') + ς('/') + ς('/') + identifier() + ς('.') + identifier() + ς('/') + identifier() + ς('/') + identifier() + ς('/') + identifier() + ς('/') + identifier() + ς('/') + identifier() + ς('/') + ς('source') + ς('/') + identifier() + ς('.') + identifier() + η() +
 ς('doc_url') + σ(':') + ς('https') + ς(':') + ς('/') + ς('/') + identifier() + ς('.') + identifier() + ς('.') + identifier() + ς('/') + identifier() + ς('/') + identifier() + ς('/') + η() +
@@ -146,12 +146,12 @@ hashStyleComment() +
 ς('license') + σ(':') + SPAN(_UCASE) + identifier() + ς('-') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + η() +
 ς('license_file') + σ(':') + SPAN(_UCASE) + η() +
 ς('summary') + σ(':') + SPAN(_UCASE) + identifier() + identifier() + identifier() + identifier() + η() +
-ς('extra') + σ(':') + η() +
+yamlStatement() + η() +
 ς('copy_test_source_files') + σ(':') + identifier() + η() +
 identifier() + ς('-') + ς('name') + ς(':') + identifier() + η() +
 ς('final') + σ(':') + identifier() + η() +
 ς('flow_run_id') + σ(':') + identifier() + ς('.') + SPAN(_DIGITS) + ς('.') + SPAN(_DIGITS) + η() +
-identifier() + ς('-') + ς('maintainers') + σ(':') + η() +
+yamlStatement() + η() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + η() +
 ς('-') + identifier() + η() +
