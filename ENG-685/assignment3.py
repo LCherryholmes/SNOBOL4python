1f4630500ec7d3c67830ca2253aaff7fd99c47e4
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-03-07 15:28:26 -0600

    Create assignment3.py

---
+++ ENG-685/assignment3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ENG 685, Lon Jones Cherryholmes, VBG Exercise
+from SNOBOL4python import GLOBALS, REPLACE, pattern
+from SNOBOL4python import _ALPHABET, _UCASE, _LCASE, _DIGITS
+from SNOBOL4python import ε, σ, Σ, Π, λ, Λ
+from SNOBOL4python import ANY, ARB, ARBNO, BAL, BREAK
+from SNOBOL4python import FENCE, LEN, POS, RPOS, SPAN
+#------------------------------------------------------------------------------
+GLOBALS(globals())
+with open("VBGinTASA.txt", "r") as trees_file:
+	trees_source = trees_file.read()
+	if trees_source in POS(0) + BAL() + RPOS(0):
+		print("Balanced!")
+	if trees_source in POS(0) + σ('(') + BAL() % "sentence"  + σ(')'):
+		print(sentence)
