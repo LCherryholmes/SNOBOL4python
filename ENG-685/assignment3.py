d5ec3d7138dfe8d002650ab86d3e839f969cedf8
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-03-07 19:30:27 -0600

    Update assignment3.py

--- ENG-685/assignment3.py
+++ ENG-685/assignment3.py
@@ -1,58 +1,66 @@
 # -*- coding: utf-8 -*-
-# ENG 685, Lon Cherryholmes Sr., VBG Exercise
+# ENG 685, VBG Exercise, Lon Cherryholmes Sr.
 from pprint import pprint
 from SNOBOL4python import GLOBALS, REPLACE, pattern
 from SNOBOL4python import _ALPHABET, _UCASE, _LCASE, _DIGITS
 from SNOBOL4python import ε, σ, Σ, Π, λ, Λ
 from SNOBOL4python import ANY, ARB, ARBNO, BAL, BREAK
-from SNOBOL4python import FENCE, LEN, POS, RPOS, SPAN
-from SNOBOL4python import nPush, nPop, nInc, Shift, Reduce
+from SNOBOL4python import FENCE, LEN, NOTANY, POS, RPOS, SPAN
 #------------------------------------------------------------------------------
 @pattern
-def delim():    yield from  SPAN(" \n")
+def delim(): yield from SPAN(" \n")
 #------------------------------------------------------------------------------
 @pattern
-def word():     yield from  BREAK('( )\n')
+def word(): yield from NOTANY("( )\n") + BREAK("( )\n")
 #------------------------------------------------------------------------------
 @pattern
-def group():    yield from  ( σ('(')
-                            + nPush()
-                            + word() % "tx" + Shift("tag", "tx") + nInc()
-                            + Λ("count_tag(tx)")
-                            + ARBNO(
-                                delim() 
-                              + ( group()
-                                | word() % "tx" + Shift("word", "tx")
-                                ) + nInc()
-                              )
-                            + Reduce('Part')
-                            + nPop()
-                            + σ(')')
-                            )
+def group():
+    yield from  ( σ('(')
+                + word() % "tag"
+                + Λ("count_tag(tag)")
+                + Λ("stack.append(list())")
+                + Λ("stack[-1].append(tag)")
+                + ARBNO(
+                    delim() 
+                  + ( group()
+                    | word() % "word" + Λ("stack[-1].append(word)")
+                    )
+                  )
+                + Λ("top = stack.pop()")
+                + Λ("if len(stack) == 0: bank = top")
+                + Λ("if len(stack) > 0: stack[-1].append(top)")
+                + σ(')')
+                )
 #------------------------------------------------------------------------------
 @pattern
-def groups():   yield from  ( POS(0)
-                            + Λ("tags = dict()")
-                            + nPush()
-                            + ARBNO(
-                                nPush()
-                              + ARBNO(group() + nInc())
-                              + delim()
-                              + Reduce('Sentence')
-                              + nPop()
-                              + nInc()
-                              )
-                            + Reduce('Sentences')
-                            + nPop()
-                            + RPOS(0)
-                            )
+def groups():
+    yield from  ( POS(0)
+                + Λ("tags = dict()")
+                + Λ("bank = None")
+                + Λ("stack = []")
+                + ARBNO(ARBNO(group()) + delim())
+                + RPOS(0)
+                )
 #------------------------------------------------------------------------------
 def count_tag(tag):
     if tag not in tags:
-        tags[tag] = 0
+        tags[tag] = 1
     else: tags[tag] += 1
 #------------------------------------------------------------------------------
 GLOBALS(globals())
+"""(S (S (NP (NN none)) (ADVP (RB ever)) (VP (VBN penned) (NP (DT a) (NN
+manifesto)) (PP (IN as) (S (VP (VBG stirring) (PP (IN as) (NP (NP (DT the)
+(NN one)) (SBAR (WHNP (WDT that)) (S (VP (VBD appeared) (PP (IN in) (NP (NP
+(DT the) (JJ first) (NN issue)) (PP (IN of) (NP (DT the) (NN
+liberator))))))))))))))) (, ,) (CC and) (S (NP (DT no) (JJ other) (NN
+abolitionist) (NN document)) (VP (VBZ is) (ADVP (RB so) (RB well)) (VP (VBN
+remembered)))) (.  .))
+""" in groups()
+pprint(bank, indent=2)
+print()
+pprint(tags, indent=2)
+exit()
+#------------------------------------------------------------------------------
 with open("VBGinTASA.txt", "r") as trees_file:
     trees_source = trees_file.read()
     if trees_source in (POS(0) + BAL() + RPOS(0)):
@@ -60,6 +68,6 @@ with open("VBGinTASA.txt", "r") as trees_file:
     if trees_source in groups():
         print("Yeah!")
         print(tags)
-        sentences_tree = vstack.pop()
-        pprint(sentences_tree)
+        print(bank)
     else: print("Boo!")
+#------------------------------------------------------------------------------
