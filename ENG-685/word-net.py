180102b0bea5c045ed947563428f6ba46d6fe340
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-03-13 11:46:23 -0500

    Update word-net.py

--- ENG-685/word-net.py
+++ ENG-685/word-net.py
@@ -11,55 +11,62 @@ hex2Dec = {
      '8': 8,  '9': 9,  'a': 10, 'b': 11,
      'c': 12, 'd': 13, 'e': 14, 'f': 15}
 #-------------------------------------------------------------------------------
-GLOBALS(globals())
-lexicon = dict()
+lineno = 0
+def inc(): global lineno; lineno += 1; return True
+#-------------------------------------------------------------------------------
 lexs = {"noun", "verb", "adj", "adv"}
+lexicon = dict()
+def enter(word, dict_type):
+    global lexicon
+    word = REPLACE(word, '_', ' ')
+    if word in lexicon:
+        if dict_type not in lexicon[word]:
+            lexicon[word] += dict_type
+    else: lexicon[word] = dict_type 
+    if lineno % 10_000 == 0:
+        print(lineno, word, lexicon[word])
+    return True
+#-------------------------------------------------------------------------------
+GLOBALS(globals())
 for lex in lexs:
     file_name = "C:/nltk_data/corpora/wordnet2022/data." + lex
     print(f"Reading: {file_name}")
-    with open(file_name, "r") as file:
-        line = file.readline()
-        while line in (POS(0) + σ('  ') + SPAN('0123456789') + σ(' ')):
-            line = file.readline()
-        while line in   ( POS(0)
-                        + SPAN('0123456789')
-                        + σ(' ') + SPAN('0123456789')
-                        + σ(' ') + ANY('asrnv') % "dict_type"
-                        + σ(' ') + SPAN('0123456789abcdef') % "nhex"
-                        + σ(' ') + REM() % "line"
-                        + RPOS(0)
-                        ):
-#           --------------------------------------------------------------------
-            n_entries = hex2Dec[nhex[0]] * 16 + hex2Dec[nhex[1]]
-            for n in range(n_entries):
-#               ----------------------------------------------------------------
-                if line not in  ( POS(0)
-                                + BREAK(' (') % "word"
-                                + σ('(') + BREAK(')') + σ(') ')
-                                + REM() % "line"
-                                + RPOS(0)
-                                ):
-                    if line in  ( POS(0)
-                                + BREAK(' (') % "word"
-                                + σ(' ')
-                                + REM() % "line"
-                                + RPOS(0)
-                                ):
-#                       word = REPLACE(word, '_', ' ')
-                        if word in lexicon:
-                            if dict_type not in lexicon[word]:
-                                lexicon[word] += dict_type
-                        else: lexicon[word] = dict_type 
-                        print(f'"{word}"={lexicon[word]}', end=" ")
-#               ----------------------------------------------------------------
-                if line not in  ( POS(0)
-                                + SPAN('0123456789abcdef') + σ(' ')
-                                + REM() % "line"
-                                + RPOS(0)
-                                ):
-                    raise Exception("Never say never!")
-#           --------------------------------------------------------------------
-            line = file.readline()
+#   ----------------------------------------------------------------------------
+    with open(file_name, "r", encoding="utf-8") as file:
+        lex_info = file.read()
+#       ------------------------------------------------------------------------
+        print(f"Parsing ....")
+        lineno = 0
+        if lex_info in \
+            ( POS(0)
+            + ARBNO(
+                σ('  ') + SPAN('0123456789')
+              + σ(' ') + BREAK("\n")
+              + σ('\n')
+              + λ(lambda: inc())
+              )
+            + ARBNO(
+                SPAN('0123456789')
+              + σ(' ') + SPAN('0123456789')
+              + σ(' ') + ANY('asrnv') @ "dict_type"
+              + σ(' ') + SPAN('0123456789abcdef') @ "nhex"
+              + σ(' ')
+              + ARBNO(
+                  BREAK(' (') @ "word" + λ(lambda: enter(word, dict_type))
+                + (σ(' ') | σ('(') + BREAK(')') + σ(') '))
+                + SPAN('0123456789abcdef') + σ(' ')
+                )
+              + ANY('0123456789')
+              + ANY('0123456789')
+              + ANY('0123456789')
+              + σ(' ')
+              + BREAK("\n")
+              + σ('\n')
+              + λ(lambda: inc())
+              )
+            + RPOS(0)
+            ):
+            print("#" * 80)
 #-------------------------------------------------------------------------------
 pprint(lexicon)
 #-------------------------------------------------------------------------------
