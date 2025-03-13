be441ee276a9a6d07935c7b448ae59b117f74771
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-03-13 14:44:49 -0500

    Update word-net.py

--- ENG-685/word-net.py
+++ ENG-685/word-net.py
@@ -5,21 +5,15 @@ from SNOBOL4python import _ALPHABET, _UCASE, _LCASE, _DIGITS, REPLACE
 from SNOBOL4python import ANY, ARBNO, BAL, BREAK, LEN, NOTANY, POS
 from SNOBOL4python import REM, RPOS, SPAN
 #-------------------------------------------------------------------------------
-hex2Dec = {
-     '0': 0,  '1': 1,  '2': 2,  '3': 3,
-     '4': 4,  '5': 5,  '6': 6,  '7': 7,
-     '8': 8,  '9': 9,  'a': 10, 'b': 11,
-     'c': 12, 'd': 13, 'e': 14, 'f': 15}
-#-------------------------------------------------------------------------------
 lineno = 0
 def inc():
     global lineno
+    lineno += 1
     if lineno % 10_000 == 0:
         print(lineno)
-    lineno += 1
     return True
 #-------------------------------------------------------------------------------
-lexs = {"noun", "verb", "adj", "adv"}
+lexs = ("adv", "verb", "adj", "noun")
 lexicon = dict()
 def enter(word, dict_type):
     global lexicon
@@ -30,46 +24,87 @@ def enter(word, dict_type):
     else: lexicon[word] = dict_type 
     return True
 #-------------------------------------------------------------------------------
+@pattern
+def sno_wordnet():
+    yield from  ( POS(0)
+                + ARBNO( # header / license banner
+                    σ('  ') + SPAN('0123456789')
+                  + σ(' ') + BREAK("\n")
+                  + λ(lambda: inc())
+                  + σ('\n')
+                  )
+                + ARBNO( # data rows
+                    SPAN('0123456789')
+                  + σ(' ') + SPAN('0123456789')
+                  + σ(' ') + ANY('asrnv') % "dict_type"
+                  + σ(' ') + SPAN('0123456789abcdef') @ "nhex"
+                  + σ(' ')
+                  + ARBNO(
+                      BREAK(' (') % "word" + Λ("enter(word, dict_type)")
+                    + (σ(' ') | σ('(') + BREAK(')') + σ(') '))
+                    + SPAN('0123456789abcdef') + σ(' ')
+                    )
+                  + ANY('0123456789')
+                  + ANY('0123456789')
+                  + ANY('0123456789')
+                  + σ(' ')
+                  + BREAK("\n")
+                  + λ(lambda: inc())
+                  + σ('\n')
+                  )
+                + RPOS(0)
+                )
+#-------------------------------------------------------------------------------
+import re
+re_word_id = re.compile(r" ([^ ]+) [0123456789abcdef]{1,2}")
+re_wordnet = re.compile(
+    r"^[0123456789]{8}"
+    r" [0123456789]{2}"
+    r" ([asrnv])"
+    r" [0123456789abcdef]{2}"
+    r"( [^ ]+ [0123456789abcdef]{1,2})+"
+    r" [0123456789]{3}"
+     "("
+        r" (?:\$|\\|\+|\*|<|=|&|!|-c|-r|-u|#m|#p|#s|~i|%m|%p|%s|;c|;r|;u|~|@|@i|>|\^)"
+        r" [0123456789]{8}"
+        r" [anrsv]"
+        r" [0123456789abcdef]{4}"
+     ")*"
+    r"( [0123456789]{2}( \+( [0123456789abcdef]{2})*)*)?"
+    r" \| .*\n$"
+)
+#-------------------------------------------------------------------------------
+for lex in lexs:
+    file_name = "C:/nltk_data/corpora/wordnet2022/data." + lex
+    print(f"Reading: {file_name}")
+    with open(file_name, "r", encoding="utf-8") as file:
+        lineno = 0
+        while lineno < 30:
+            line = file.readline()
+            lineno += 1
+        while line := file.readline():
+            lineno += 1
+            wordnet = re.fullmatch(re_wordnet, line)
+            if wordnet:
+                dict_type = wordnet.group(1)
+                word_list = wordnet.group(2)
+                for word_id in re.finditer(re_word_id, word_list):
+                    word = word_id.group(1)
+                    enter(word, dict_type)
+            elif line != "\n":
+                print(f'{lineno}: <<<{line}>>>')
+                exit()
+print(len(lexicon))
+exit()
+#-------------------------------------------------------------------------------
 GLOBALS(globals())
 for lex in lexs:
     file_name = "C:/nltk_data/corpora/wordnet2022/data." + lex
     print(f"Reading: {file_name}")
-#   ----------------------------------------------------------------------------
     with open(file_name, "r", encoding="utf-8") as file:
         lex_info = file.read()
-#       ------------------------------------------------------------------------
         print(f"Parsing ....")
         lineno = 0
-        if lex_info in \
-            ( POS(0)
-            + ARBNO(
-                σ('  ') + SPAN('0123456789')
-              + σ(' ') + BREAK("\n")
-              + λ(lambda: inc())
-              + σ('\n')
-              )
-            + ARBNO(
-                SPAN('0123456789')
-              + σ(' ') + SPAN('0123456789')
-              + σ(' ') + ANY('asrnv') # @ "dict_type"
-              + σ(' ') + SPAN('0123456789abcdef') # @ "nhex"
-              + σ(' ')
-              + ARBNO(
-                  BREAK(' (') @ "word" # + λ(lambda: enter(word, dict_type))
-                + (σ(' ') | σ('(') + BREAK(')') + σ(') '))
-                + SPAN('0123456789abcdef') + σ(' ')
-                )
-              + ANY('0123456789')
-              + ANY('0123456789')
-              + ANY('0123456789')
-              + σ(' ')
-              + BREAK("\n")
-              + λ(lambda: inc())
-              + σ('\n')
-              )
-            + RPOS(0)
-            ):
+        if lex_info in sno_wordnet():
             print("#" * 80)
-#-------------------------------------------------------------------------------
-pprint(lexicon)
 #-------------------------------------------------------------------------------
