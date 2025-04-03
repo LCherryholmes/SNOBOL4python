9da1d65a6cda553cc91d11ccc5c65d3b1d2226dd
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-04-02 22:48:54 -0500

    SNOBOL4 parser written in Python.

--- src/SNOBOL4python/__init__.py
+++ src/SNOBOL4python/__init__.py
@@ -4,13 +4,13 @@ from .SNOBOL4patterns import ε, σ, π, λ, Λ, θ, Θ, φ, Φ, α, ω
 from .SNOBOL4patterns import ABORT, ANY, ARB, ARBNO, BAL, BREAK, BREAKX, FAIL
 from .SNOBOL4patterns import FENCE, LEN, MARB, MARBNO, NOTANY, POS, REM, RPOS
 from .SNOBOL4patterns import RTAB, SPAN, SUCCESS, TAB
+from .SNOBOL4functions import ALPHABET, DIGITS, UCASE, LCASE
 from .SNOBOL4patterns import nPush, nInc, nPop, Shift, Reduce, Pop
 #-------------------------------------------------------------------------------
 from .SNOBOL4patterns import PATTERN, SEARCH, MATCH, FULLMATCH
 from .SNOBOL4patterns import Σ, Π, ξ, Ω, Δ, δ
 from .SNOBOL4patterns import ψ, Ψ, Ϙ # for future enhancement
 #-------------------------------------------------------------------------------
-from .SNOBOL4functions import ALPHABET, DIGITS, UCASE, LCASE
 from .SNOBOL4functions import CHAR, DIFFER, IDENT, INTEGER, REPLACE
 from .SNOBOL4functions import JSONDecode
 from .SNOBOL4functions import S, F, Ξ, _END, _RETURN, _FRETURN, _NRETURN
@@ -22,7 +22,7 @@ __all__ = [
             "GLOBALS", "pattern",
             "ε", "σ", "π", "λ", "Λ", "θ", "Θ", "φ", "Φ", "α", "ω",
             "ABORT", "ANY", "ARB", "ARBNO", "BAL", "BREAK", "BREAKX", "FAIL",
-            "FENCE", "LEN", "MARBNO", "NOTANY", "POS", "REM", "RPOS",
+            "FENCE", "LEN", "MARB", "MARBNO", "NOTANY", "POS", "REM", "RPOS",
             "RTAB", "SPAN", "SUCCESS", "TAB",
             "nPush", "nInc", "nPop", "Shift", "Reduce", "Pop",
 
