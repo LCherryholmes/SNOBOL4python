811f5429af924954aa8098267351c79be20da13b
LCherryholmes <80133296+LCherryholmes@users.noreply.github.com>
2025-05-29 14:44:25 -0500

    beautify

--- misc/byrd_box.py
+++ misc/byrd_box.py
@@ -13,68 +13,66 @@ TRACE(40)
 #------------------------------------------------------------------------------
 icon_source = "every write(5 > ((1 to 2) * (3 to 4)));"
 #------------------------------------------------------------------------------
-η           =   SPAN(" \t\r\n") | ε()
-def ς(s):       return η + σ(s) @ "text"
-integer     =   η + SPAN(DIGITS) @ "text" % "value"
-word        =   η + SPAN(LCASE) @ "text" % "word"
-to          =   word + Λ(lambda: text == "to")
-every       =   word + Λ(lambda: text == "every")
-write       =   word + Λ(lambda: text == "write")
-variable    =   word + Λ(lambda: text not in ["every", "to"])
-function    =   word + Λ(lambda: text in ["write"])
+η         =   SPAN(" \t\r\n") | ε()
+def ς(s):     return η + σ(s) @ "text"
+integer   =   η + SPAN(DIGITS) @ "text" % "value"
+word      =   η + SPAN(LCASE) @ "text" % "word"
+to        =   word + Λ(lambda: text == "to")
+every     =   word + Λ(lambda: text == "every")
+write     =   word + Λ(lambda: text == "write")
+variable  =   word + Λ(lambda: text not in ["every", "to"])
+function  =   word + Λ(lambda: text in ["write"])
 #------------------------------------------------------------------------------
-expression6 =   ( ς('(') + ζ("expression1") + ς(')')
-                | write + ς('(') + ζ("expression1") + ς(')') + Reduce("WRITE", 1)
-                | variable + Shift("V", "word")
-                | integer + Shift("I", "value")
-                )
+expr6     =   ( ς('(') + ζ("expr1") + ς(')')
+              | write + ς('(') + ζ("expr1") + ς(')') + Reduce("WRITE", 1)
+              | variable + Shift("V", "word")
+              | integer + Shift("I", "value")
+              )
 #------------------------------------------------------------------------------
-expression5 =   ( ς('+') + ζ("expression5") + Reduce('+', 1)
-                | ς('-') + ζ("expression5") + Reduce('-', 1)
-                | expression6
-                )
+expr5     =   ( ς('+') + ζ("expr5") + Reduce('+', 1)
+              | ς('-') + ζ("expr5") + Reduce('-', 1)
+              | expr6
+              )
 #------------------------------------------------------------------------------
-expression4 =   ( expression5
-                + ( ς('*') + ζ("expression4") + Reduce('*', 2)
-                  | ς('/') + ζ("expression4") + Reduce('/', 2)
-                  | ε()
-                  )
+expr4     =   ( expr5
+              + ( ς('*') + ζ("expr4") + Reduce('*', 2)
+                | ς('/') + ζ("expr4") + Reduce('/', 2)
+                | ε()
                 )
+              )
 #------------------------------------------------------------------------------
-expression3  =  ( expression4
-                + ( ς('+') + ζ("expression3") + Reduce('+', 2)
-                  | ς('-') + ζ("expression3") + Reduce('-', 2)
-                  | ε()
-                  )
+expr3     =   ( expr4
+              + ( ς('+') + ζ("expr3") + Reduce('+', 2)
+                | ς('-') + ζ("expr3") + Reduce('-', 2)
+                | ε()
                 )
+              )
 #------------------------------------------------------------------------------
-expression2  =  ( expression3 + to + expression3 + Reduce('TO', 2)
-                | expression3
-                )
+expr2     =   ( expr3 + to + expr3 + Reduce('TO', 2)
+              | expr3
+              )
 #------------------------------------------------------------------------------
-expression1 =   ( expression2
-                + ( ς('<')  + expression2 + Reduce('<', 2)
-                  | ς('>')  + expression2 + Reduce('>', 2)
-                  | ς('==') + expression2 + Reduce('==', 2)
-                  | ς('<=') + expression2 + Reduce('<=', 2)
-                  | ς('>=') + expression2 + Reduce('<=', 2)
-                  | ς('!=') + expression2 + Reduce('!=', 2)
-                  | ε()
-                  )
+expr1     =   ( expr2
+              + ( ς('<')  + expr2 + Reduce('<', 2)
+                | ς('>')  + expr2 + Reduce('>', 2)
+                | ς('==') + expr2 + Reduce('==', 2)
+                | ς('<=') + expr2 + Reduce('<=', 2)
+                | ς('>=') + expr2 + Reduce('<=', 2)
+                | ς('!=') + expr2 + Reduce('!=', 2)
+                | ε()
                 )
+              )
 #------------------------------------------------------------------------------
-statement   =   ( every + expression1 + Reduce('EVERY', 1)
-                | expression1
-                )
+stmt        =   every + expr1 + Reduce('EVERY', 1) | expr1
 #------------------------------------------------------------------------------
 program     =   ( POS(0)
-                + nPush()
-                + ARBNO(statement + nInc() + ς(';'))
-                + Reduce("ICON")
-                + nPop()
-                + Pop('icon')
-                + RPOS(0)
-                )
+              + nPush()
+              + ARBNO(stmt + nInc() + ς(';'))
+              + Reduce("ICON")
+              + nPop()
+              + Pop('icon')
+              + RPOS(0)
+              )
 #------------------------------------------------------------------------------
 if icon_source in program:
     pprint(icon)
